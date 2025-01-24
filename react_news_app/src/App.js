88100f2fd8b681f7061e98ed1b6f6dc2bb615f5c
Shikhar Dave <b22ch032@iitj.ac.in>
2025-01-25 02:56:25 +0530

    components

--- react_news_app/src/App.js
+++ react_news_app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello , this is Shikhar Dave 
+        <NavBar/>
+        <News/> 
       </div>
     )
   }
