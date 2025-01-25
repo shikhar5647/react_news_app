16fbbdc435d7093881ab4c1819c55288a8b76e15
Shikhar Dave <b22ch032@iitj.ac.in>
2025-01-26 00:48:07 +0530

    items

--- react_news_app/src/components/NewsItem.js
+++ react_news_app/src/components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <h5 class="card-title">{title}</h5>
+                <p class="card-text">{description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
