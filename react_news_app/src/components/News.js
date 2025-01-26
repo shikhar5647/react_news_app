d9ab4bb51c4f710c12f2ade778a1806f70400d7b
Shikhar Dave <b22ch032@iitj.ac.in>
2025-01-26 12:03:31 +0530

    component_did_mount

--- react_news_app/src/components/News.js
+++ react_news_app/src/components/News.js
@@ -11,6 +11,24 @@ export class News extends Component {
     };
   }
 
+  componentDidMount() {
+    let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=e3816982035742d6b4beb5a4ab9b89a9';
+    this.setState({ loading: true });
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          articles: data.articles,
+          totalResults: data.totalResults,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div className="container my-3"> 
